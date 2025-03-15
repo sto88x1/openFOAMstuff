c785f79c5e8970ba0f3a21ce03ca99fbe2d72255
Sto88 <sergio.tosi88@gmail.com>
2025-03-15 20:44:35 +0100

    Add files via upload

---
+++ AMR/runSim.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+### RUN OPENFOAM ###
+. /usr/lib/openfoam/openfoam2212/etc/bashrc
+
+cd surfaceMeshes
+surfaceAdd inlet.stl wall.stl geometry.stl
+surfaceAdd outlet.stl geometry.stl geometry.stl
+#surfacePointMerge geometry.stl 0.001 geometry.stl
+#surfaceTransformPoints -write-scale 0.001 geometry.stl geometry.stl
+surfaceFeatureEdges -angle 45 geometry.stl geometry.fms
+cd ..
+cartesianMesh | tee log.cartesianMesh
+checkMesh | tee log.checkMesh
+
+. /usr/lib/openfoam/openfoam2412/etc/bashrc
+decomposePar
+mpirun -np 6 simpleFoam -parallel | tee log.simpleFoam
+reconstructParMesh
+reconstructPar
+rm -r proc*
